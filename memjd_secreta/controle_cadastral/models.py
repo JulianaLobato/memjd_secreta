f1d3da4e859bb448d5dbad9a60fd3cc7c389167d
JULIANA LOBATO DA SILVA <juliana.lobato26@gmail.com>
2018-10-19 19:27:52 -0300

    first push

--- memjd_secreta/controle_cadastral/models.py
+++ memjd_secreta/controle_cadastral/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    nome = models.CharField(
+        max_length = 255,  
+    )    
+    def __str__(self):
+        return self.nome
+
+
+class Atividade(models.Model):
+    tema = models.CharField(
+        max_length = 255,  
+    )
+    palestrante = models.CharField(
+        max_length = 255,
+    )
+    data = models.DateField()   
+    ciclo = models.IntegerField()
+    observacao = models.TextField(
+        null = True,
+        blank = True
+    )
+
+class Integrante(models.Model):
+    nome = models.CharField(
+        max_length = 255,
+        null = False,
+        blank = False    
+    )
+    nascimento = models.DateField(
+        null = False,
+        blank = False  
+    )
+    email = models.EmailField(
+        null = False,
+        blank = False  
+    )
+    telefone = models.CharField(
+        max_length=255, 
+        verbose_name=u'Telefone',                                
+        default='0'
+    )
+    celular = models.CharField(
+        max_length=255, 
+        verbose_name='Celular',                                
+        default='0'
+    )   
+    tarefa = models.ForeignKey(
+        Tarefa, 
+        on_delete=models.SET_NULL, 
+        null=True
+    )
+    observacao = models.TextField(
+        null = True,
+        blank = True
+    )
+    doenca = models.TextField(
+        null = True,
+        blank = True
+    )    
--- memjd_secreta/memjd_secreta/urls.py
+++ memjd_secreta/memjd_secreta/urls.py
@@ -1,21 +1,34 @@
-"""
-Definition of urls for memjd_secreta.
-"""
-
 from django.conf.urls import include, url
+import django.contrib.auth.views
+import controle_cadastral.views
+import controle_cadastral.forms
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# Enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', memjd_secreta.views.home, name='home'),
-    # url(r'^memjd_secreta/', include('memjd_secreta.memjd_secreta.urls')),
+    url(r'^$', controle_cadastral.views.index, name='index'),
+   # url(r'^memjd_secreta/', include('memjd_secreta.memjd_secreta.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'controle_cadastral/login.html',
+            'authentication_form': controle_cadastral.forms.BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title': 'Entrar',
+            },
+            'next_page': '/admin',
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.logout,
+        {
+            'next_page': '/index',
+        },
+        name='logout'),
 ]
